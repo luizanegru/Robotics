8752b8130fad4eaaba4f2fff78d154e15fb475e7
luizanegru <46330271+luizanegru@users.noreply.github.com>
2019-10-28 22:36:27 +0200

    1st Homework
    Control a RGB LED through 3 potentiometers for each color

---
+++ Laboratory homeworks/RGB-potControl.ino
@@ -0,0 +1,49 @@
+int potRedValue = 0;
+int potGreenValue = 0;
+int potBlueValue = 0;
+
+int ledRedValue = 0;
+int ledGreenValue = 0;
+int ledBlueValue = 0;
+
+const int potPinRed = A0;
+const int potPinGreen = A1;
+const int potPinBlue = A2;
+
+
+const int ledPinRed = 11;
+const int ledPinGreen = 10;
+const int ledPinBlue = 9;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  potRedValue = analogRead(potPinRed);
+  potGreenValue = analogRead(potPinGreen);
+  potBlueValue = analogRead(potPinBlue);
+
+  ledRedValue = map(potRedValue, 0, 1023, 0, 255);
+  ledGreenValue = map(potGreenValue, 0, 1023, 0, 255);
+  ledBlueValue = map(potBlueValue, 0, 1023, 0, 255);
+  
+
+  
+  analogWrite(ledPinRed, potRedValue);
+  analogWrite(ledPinGreen, potGreenValue);
+  analogWrite(ledPinBlue, potBlueValue);
+
+  Serial.print("Pot1: ");
+  Serial.println(potRedValue);
+  
+  Serial.print("Pot2: ");
+  Serial.println(potGreenValue);
+
+  Serial.print("Pot3: ");
+  Serial.println(potBlueValue);
+  
+  delay(1);
+
+}
